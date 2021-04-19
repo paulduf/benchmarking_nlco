c1103b9daae7004ee84d6ee7f4be3ed0d78acba9
DUFOSSE Paul <paul.dufosse@inria.fr>
2021-04-19 15:16:42 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nlcco",
+    version="0.0.1",
+    author="Paul Dufossé",
+    author_email="paul.dufosse@inria.fr",
+    description="Benchmark Problems for Non Linear Constrained Optimization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    requires=["numpy", "matplotlib"],
+    python_requires='>=3.6',
+)
